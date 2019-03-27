5e82d395004227f564f33d1d88b9e6c044939269
Student <student@fvtc.edu>
2019-03-26 20:15:37 -0500

    Worked on the CRUD methods for Band

--- TSS.Band.UI/Controllers/BandController.cs
+++ TSS.Band.UI/Controllers/BandController.cs
@@ -22,6 +22,9 @@ namespace TSS.Band.UI.Controllers
         public ActionResult Index()
         {
             // Index  = list of things 
+            if (Session["bands"] != null)
+                bands = (BandModel[])Session["bands"];
+
             return View(bands);
         }
 
@@ -29,6 +32,63 @@ namespace TSS.Band.UI.Controllers
         {
             var band = bands.FirstOrDefault(p => p.Id == id);
             return View(band);
+        }
+
+
+        // POST/GET Create()
+        // GET; default
+        public ActionResult Create()
+        {
+            BandModel band = new BandModel();
+            return View(band);
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Create(BandModel band)
+        {
+            // Add the new band to the array of Bands 
+            Array.Resize(ref bands, bands.Length + 1);
+            //band.Id = bands.Max(b => b.Id + 1);
+            band.Id = bands.Length + 1;
+            bands[bands.Length - 1] = band;
+            Session["bands"] = bands;
+            return RedirectToAction("Index");
+        }
+
+
+        // POST/GET Edit()
+        public ActionResult Edit(int id)
+        {
+            var band = bands.FirstOrDefault(b => b.Id == id);
+            return View(band);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, BandModel band)
+        {
+            bands[id - 1] = band;
+            Session["bands"] = bands;
+            return RedirectToAction("Index");
+        }
+
+
+        // POST/GET Delete()
+        public ActionResult Delete(int id)
+        {
+            var band = bands.FirstOrDefault(b => b.Id == id);
+            return View(band);
+        }
+
+        [HttpPost]
+        public ActionResult Delete (int id, BandModel band)
+        {
+            if (Session["bands"] != null)
+                bands = (BandModel[])Session["bands"];
+            var newbands = bands.Where(b => b.Id != id);
+            bands = newbands.ToArray();
+            Session["bands"] = bands;
+            return RedirectToAction("Index");
         }
     }
 }
